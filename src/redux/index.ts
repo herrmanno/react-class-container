5eb91f9ea980922726761c7a7fb24ef042c5809a
Oliver Herrmann <herrmann@livebook.de>
2018-07-19 12:22:29 +0200

    remove unneccesary re-export of redux

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,6 @@
-import * as redux from "redux"
 import ReduxContainer from "./ReduxContainer"
 import ReduxContainerClass from "./ReduxContainerClass"
 import ReduxContainerComponent from "./ReduxContainerComponent"
 import Provider from "./Provider"
 
-export { redux }
 export { ReduxContainer, ReduxContainerClass, ReduxContainerComponent, Provider }
