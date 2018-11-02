5bbce02f469edf76ca83bb62b0756c295d2791c9
Oliver Herrmann <herrmann@id-on.de>
2018-11-02 15:17:57 +0100

    make provider impure

--- src/redux/Provider.ts
+++ src/redux/Provider.ts
@@ -5,7 +5,7 @@ import * as Proptypes from "prop-types"
 /**
  * Provides a given Redux-Store to child components via context
  */
-class Provider extends React.PureComponent<{ store: Store<any> }, {}> {
+class Provider extends React.Component<{ store: Store<any> }, {}> {
   static childContextTypes = {
     store: Proptypes.any.isRequired
   }
